a7131426073a49a3e8017a59f5ed0ddd99980ac8
Kinder2149 <vcoutrygmail.com>
2026-01-27 20:08:08 +0100

    feat: 132

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3002/api',
   supabaseUrl: 'https://rnreaaeiccqkwgwxwxeg.supabase.co',
-  supabaseKey: 'sb_publishable_5C5PlWrOG7Krvpo6YEQZMg_rEEuKzVw'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJucmVhYWVpY2Nxa3dnd3h3eGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MzI3NjAsImV4cCI6MjA1MDMwODc2MH0.5C5PlWrOG7Krvpo6YEQZMg_rEEuKzVw'
 };
